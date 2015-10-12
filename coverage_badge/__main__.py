b277a88909f870c6aa70e2adf95996ce52707fca
Danilo Bargen <mail@dbrgn.ch>
2015-10-12 17:59:26 +0200

    Python 2 compat

--- coverage_badge/__main__.py
+++ coverage_badge/__main__.py
@@ -1,6 +1,9 @@
 """
 Generate coverage badges for Coverage.py.
 """
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
 import os
 import sys
 import argparse
@@ -8,7 +11,7 @@ import pkg_resources
 import coverage
 
 
-class Devnull:
+class Devnull(object):
     """
     A file like object that does nothing.
     """
